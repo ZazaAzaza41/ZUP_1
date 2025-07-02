ebe2d23339b73787ffcc33958853e9fdf2df9126
ZZInfaZV <zzahar052007@gmail.com>
2025-07-02 14:22:49 +0300

    fix obstacle destroy

--- Assets/Objects/EnemyObstacles/SpawnObstacles.cs
+++ Assets/Objects/EnemyObstacles/SpawnObstacles.cs
@@ -53,17 +53,17 @@ public class SpawnObstacles : MonoBehaviour
             Destroy(spawnedObject2, livetime + timespawn - 0.1f);
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject3 = Instantiate(objectlist[obj], new Vector3(2.65f,UnityEngine.Random.Range(y, miny), z), Quaternion.identity, transform);
-            Destroy(spawnedObject2, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject3, livetime + timespawn - 0.1f);
 
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject11 = Instantiate(objectlist[obj], new Vector3(x, y + UnityEngine.Random.Range(miny, miny), z), Quaternion.identity, transform);
-            Destroy(spawnedObject, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject11, livetime + timespawn - 0.1f);
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject22 = Instantiate(objectlist[obj], new Vector3(0, y + UnityEngine.Random.Range(miny, maxy), z), Quaternion.identity, transform);
-            Destroy(spawnedObject2, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject22, livetime + timespawn - 0.1f);
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject33 = Instantiate(objectlist[obj], new Vector3(2.65f, y + UnityEngine.Random.Range(miny, maxy), z), Quaternion.identity, transform);
-            Destroy(spawnedObject2, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject33, livetime + timespawn - 0.1f);
 
         }
         else if (xx == 2)
@@ -80,13 +80,13 @@ public class SpawnObstacles : MonoBehaviour
 
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject11 = Instantiate(objectlist[obj], new Vector3(x, y+UnityEngine.Random.Range(miny, maxy), z), Quaternion.identity, transform);
-            Destroy(spawnedObject, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject11, livetime + timespawn - 0.1f);
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject22 = Instantiate(objectlist[obj], new Vector3(-2.65f, y +UnityEngine.Random.Range(miny, maxy), z), Quaternion.identity, transform);
-            Destroy(spawnedObject2, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject22, livetime + timespawn - 0.1f);
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject33 = Instantiate(objectlist[obj], new Vector3(2.65f, y +UnityEngine.Random.Range(miny, maxy), z), Quaternion.identity, transform);
-            Destroy(spawnedObject3, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject33, livetime + timespawn - 0.1f);
         }
         else
         {
@@ -102,13 +102,13 @@ public class SpawnObstacles : MonoBehaviour
 
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject11 = Instantiate(objectlist[obj], new Vector3(x, y + UnityEngine.Random.Range(miny, maxy), z), Quaternion.identity, transform);
-            Destroy(spawnedObject, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject11, livetime + timespawn - 0.1f);
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject22 = Instantiate(objectlist[obj], new Vector3(-2.65f, y + UnityEngine.Random.Range(miny, maxy), z), Quaternion.identity, transform);
-            Destroy(spawnedObject2, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject22, livetime + timespawn - 0.1f);
             obj = UnityEngine.Random.Range(0, kolobstacles);
             GameObject spawnedObject33 = Instantiate(objectlist[obj], new Vector3(0f, y + UnityEngine.Random.Range(miny, maxy), z), Quaternion.identity, transform);
-            Destroy(spawnedObject3, livetime + timespawn - 0.1f);
+            Destroy(spawnedObject33, livetime + timespawn - 0.1f);
         }
     }
     void Update()
