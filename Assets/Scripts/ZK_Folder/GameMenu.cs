c8f157960fa7dfec37fbe824c9bc644b17833ad8
ZazaAzaza41 <147328198+ZazaAzaza41@users.noreply.github.com>
2025-07-04 11:42:07 +0300

    u can change ship skin

--- Assets/Scripts/ZK_Folder/GameMenu.cs
+++ Assets/Scripts/ZK_Folder/GameMenu.cs
@@ -1,25 +1,137 @@
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class GameMenu : MonoBehaviour
+//{
+//    public GameObject mainMenuPanel;
+//    public GameObject shopPanel;
+//    public Button startButton;
+//    public Button shopButton;
+//    public Button backButton;
+
+
+
+
+//    void Start()
+//    {
+//        if (mainMenuPanel != null)
+//        {
+//            mainMenuPanel.SetActive(true);
+//        }
+//        if (shopPanel != null)
+//        {
+//            shopPanel.SetActive(false);
+//        }
+//        if (startButton != null)
+//        {
+//            startButton.onClick.AddListener(StartGame);
+//        }
+//        if (shopButton != null)
+//        {
+//            shopButton.onClick.AddListener(OpenShop);
+//        }
+//        if (backButton != null)
+//        {
+//            backButton.onClick.AddListener(BackToMainMenu);
+//        }
+//    }
+
+//    void OpenShop()
+//    {
+//        mainMenuPanel.SetActive(false);
+//        shopPanel.SetActive(true);
+//    }
+
+//    void BackToMainMenu()
+//    {
+//        mainMenuPanel.SetActive(true);
+//        shopPanel.SetActive(false);
+//    }
+
+//    void StartGame()
+//    {
+//        if (mainMenuPanel != null)
+//        {
+//            mainMenuPanel.SetActive(false);
+//        }
+//    }
+
+//}
+
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject mainMenuPanel;
+    public GameObject shopPanel;
     public Button startButton;
+    public Button shopButton;
+    public Button backButton;
 
+    // Кнопки для изменения материала корабля
+    public Button purpleShipButton;
+    public Button goldShipButton;
+    public Button blackShipButton;
 
+    // Ссылка на Renderer корабля.  Назначьте его в инспекторе!
+    public Renderer shipRenderer;
+
+    // Материалы для корабля
+    public Material purpleShipMaterial;
+    public Material goldShipMaterial;
+    public Material blackShipMaterial;
 
 
     void Start()
     {
+        // Проверки на null и активация панелей
         if (mainMenuPanel != null)
         {
             mainMenuPanel.SetActive(true);
         }
-
+        if (shopPanel != null)
+        {
+            shopPanel.SetActive(false);
+        }
+        // Подписка на события кнопок
         if (startButton != null)
         {
             startButton.onClick.AddListener(StartGame);
         }
+        if (shopButton != null)
+        {
+            shopButton.onClick.AddListener(OpenShop);
+        }
+        if (backButton != null)
+        {
+            backButton.onClick.AddListener(BackToMainMenu);
+        }
+        // Подписка на события кнопок изменения материала
+        if (purpleShipButton != null)
+        {
+            purpleShipButton.onClick.AddListener(SetShipMaterialPurple);
+        }
+        if (goldShipButton != null)
+        {
+            goldShipButton.onClick.AddListener(SetShipMaterialGold);
+        }
+        if (blackShipButton != null)
+        {
+            blackShipButton.onClick.AddListener(SetShipMaterialBlack);
+        }
+    }
+    void OpenShop()
+    {
+        mainMenuPanel.SetActive(false);
+        shopPanel.SetActive(true);
+    }
+
+    void BackToMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        shopPanel.SetActive(false);
     }
 
     void StartGame()
@@ -29,5 +141,27 @@ public class GameMenu : MonoBehaviour
             mainMenuPanel.SetActive(false);
         }
     }
+    void SetShipMaterialPurple()
+    {
+        if (shipRenderer != null && purpleShipMaterial != null)
+        {
+            shipRenderer.material = purpleShipMaterial;
+        }
+    }
 
+    void SetShipMaterialGold()
+    {
+        if (shipRenderer != null && goldShipMaterial != null)
+        {
+            shipRenderer.material = goldShipMaterial;
+        }
+    }
+
+    void SetShipMaterialBlack()
+    {
+        if (shipRenderer != null && blackShipMaterial != null)
+        {
+            shipRenderer.material = blackShipMaterial;
+        }
+    }
 }
